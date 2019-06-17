437c3ae329e4fab37e5eda94b80a4d3b5caedb53
Daniel Ang <dan.justin.ang@gmail.com>
2019-06-17 11:19:51 -0400

    import css

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Provider } from "react-redux";
 import Results from "./Results";
 import store from "../store";
+import './App.css';
+
 function App() {
   return (
     <Provider store={store}>
